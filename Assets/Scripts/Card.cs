cf13aba86ffdb0ef06bf2e7285a1e911925e2dcc
Jaewon <dhwodnjs0827@naver.com>
2025-01-21 09:57:10 +0900

    init

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int idx = 0;
+
+    public GameObject front;
+    public GameObject back;
+
+    public Animator anim;
+
+    AudioSource audioSource;
+    public AudioClip clip;
+
+    public SpriteRenderer frontImage;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Setting(int number)
+    {
+        idx = number;
+        frontImage.sprite = Resources.Load<Sprite>($"rtan{idx}");
+    }
+
+    public void OpenCard()
+    {
+        if (GameManager.Instance.secondCard != null)
+        {
+            return;
+        }
+
+        audioSource.PlayOneShot(clip);
+        anim.SetBool("isOpen", true);
+        front.SetActive(true);
+        back.SetActive(false);
+
+        if (GameManager.Instance.firstCard == null)
+        {
+            GameManager.Instance.firstCard = this;
+        }
+        else
+        {
+            GameManager.Instance.secondCard = this;
+            GameManager.Instance.Matched();
+        }
+    }
+
+    public void DestroyCard()
+    {
+        Invoke("DestroyCardInvoke", 1f);
+    }
+
+    void DestroyCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+
+    public void CloseCard()
+    {
+        Invoke("CloseCardInvoke", 1f);
+    }
+
+    void CloseCardInvoke()
+    {
+        anim.SetBool("isOpen", false);
+        front.SetActive(false);
+        back.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public Card firstCard;
+    public Card secondCard;
+
+    public Text timeTxt;
+    public GameObject endTxt;
+
+    AudioSource audioSource;
+    public AudioClip clip;
+
+    public int cardCount = 0;
+    float time = 0f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
+
+        if (time >= 30f)
+        {
+            timeTxt.text = 30f.ToString("N2");
+            Time.timeScale = 0f;
+            endTxt.SetActive(true);
+        }
+    }
+
+    public void Matched()
+    {
+        if (firstCard.idx == secondCard.idx)
+        {
+            audioSource.PlayOneShot(clip);
+            firstCard.DestroyCard();
+            secondCard.DestroyCard();
+            cardCount -= 2;
+            if (cardCount == 0)
+            {
+                Time.timeScale = 0f;
+                endTxt.SetActive(true);
+            }
+        }
+        else
+        {
+            firstCard.CloseCard();
+            secondCard.CloseCard();
+        }
+
+        firstCard = null;
+        secondCard = null;
+    }
+}
