b2dba84a2f8e863c775c35407c1146cd6c206714
bnm21105 <bnm2513@hanmail.net>
2025-01-22 17:49:34 +0900

    hiddentrigger

--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class StartMenu : MonoBehaviour
 {
     [SerializeField] private GameObject levelSelectMenu;
+    [SerializeField] private GameObject HiddenlevelSelectMenu;
     [SerializeField] private GameObject optionMenu;
 
     private void Start()
@@ -16,7 +17,15 @@ public class StartMenu : MonoBehaviour
 
     public void StartGame()
     {
-        UIManager.Instance.OpenUI(levelSelectMenu);
+        if (GameManager.Instance.HiddenClear)
+        {
+            UIManager.Instance.OpenUI(HiddenlevelSelectMenu);
+        }
+        else
+        {
+            UIManager.Instance.OpenUI(levelSelectMenu);
+        }
+        
     }
 
     public void DisplayOption()
