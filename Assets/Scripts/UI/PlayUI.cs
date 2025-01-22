bcb5ffa8941f8afbdd42d8cc9771cc4d5f1d19c0
leewootak <leewt32414@gmail.com>
2025-01-22 11:04:47 +0900

    색상 변경 추가

--- Assets/Scripts/UI/PlayUI.cs
+++ Assets/Scripts/UI/PlayUI.cs
@@ -6,18 +6,28 @@ using UnityEngine.UI;
 public class PlayUI : MonoBehaviour
 {
     float time = 0f;
+    public float maxTime = 30f;
     public Text timeTxt;
+    public Slider tSlider;
+    public Image sliderFill;
     public GameObject failTxt, clearTxt, normalSuccessPanel, hardSuccessPanel, board;
 
     private void Start()
     {
         Time.timeScale = 1f;
+        tSlider.maxValue = maxTime;
+        sliderFill = tSlider.fillRect.GetComponent<Image>();
     }
 
     private void Update()
     {
         time += Time.deltaTime;
         timeTxt.text = time.ToString("N2");
+        tSlider.value = time;
+
+        float t = Mathf.Clamp01(time / maxTime); // time을 0~1 사이 값으로 변환
+        sliderFill.color = Color.Lerp(Color.white, Color.red, t);
+        timeTxt.color = Color.Lerp(Color.white, Color.red, t);
 
         if (GameManager.Instance.IsFinished)
         {
@@ -27,12 +37,14 @@ public class PlayUI : MonoBehaviour
             board.SetActive(false);
             AudioManager.Instance.SFXList.Clear();
         }
-        else if (time >= 10f)// test�� 10�� ���� 30��
+        else if (time >= maxTime)
         {
-            timeTxt.text = 30f.ToString("N2");
+            timeTxt.text = maxTime.ToString("N2");
             Time.timeScale = 0f;
             failTxt.SetActive(true);
+            timeTxt.enabled = false;
             board.SetActive(false);
+            tSlider.gameObject.SetActive(false);
             AudioManager.Instance.SFXList.Clear();
         }
     }
