a41250c4c89f422d72a1ac789205b88debc5affd
Jaewon <dhwodnjs0827@naver.com>
2025-01-23 16:37:58 +0900

    Revert "히든 트리거"
    This reverts commit 4fd51ef54907398da65a45fa54fe4fb5d993e292.

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -158,7 +158,6 @@ public class GameManager : MonoBehaviour
 
     public void LoadScene(string sceneName)
     {
-        Time.timeScale = 1f;
         SceneManager.LoadScene(sceneName);
         cardList.Clear();
         UIManager.Instance.UIStack.Clear();
@@ -167,6 +166,8 @@ public class GameManager : MonoBehaviour
 
     public void ActiveHidden()
     {
+        Debug.Log("플래그 활성화");
         HiddenClear = true;
+        AudioManager.Instance.HiddenBGM();
     }
 }
